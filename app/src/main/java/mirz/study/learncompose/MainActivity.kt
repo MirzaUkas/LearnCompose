b1f3fd174fde783e5bcedb8016d6f0028b012c5d
Mirza Ukasyah <mirzauksyh@gmail.com>
2022-09-26 12:36:13 +0700

    Modifying Composable Using Modifier

--- app/src/main/java/mirz/study/learncompose/MainActivity.kt
+++ app/src/main/java/mirz/study/learncompose/MainActivity.kt
@@ -3,13 +3,18 @@ package mirz.study.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import mirz.study.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    GreetingText("World")
                 }
             }
         }
@@ -30,14 +35,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GreetingText(name: String) {
+    Text(
+        text = "Hello $name!", modifier = Modifier
+            .height(100.dp)
+            .width(200.dp)
+            .padding(8.dp)
+            .clickable {  }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LearnComposeTheme {
-        Greeting("Android")
+        GreetingText("World")
     }
 }
