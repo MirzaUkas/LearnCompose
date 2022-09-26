296b9c013cdd4189cd90aef1af4675e5ea19350c
Mirza Ukasyah <mirzauksyh@gmail.com>
2022-09-26 12:42:20 +0700

    Customizing Text Composable

--- app/src/main/java/mirz/study/learncompose/MainActivity.kt
+++ app/src/main/java/mirz/study/learncompose/MainActivity.kt
@@ -13,8 +13,12 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import mirz.study.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,7 +45,12 @@ fun GreetingText(name: String) {
             .height(100.dp)
             .width(200.dp)
             .padding(8.dp)
-            .clickable {  }
+            .clickable { },
+        style = TextStyle(
+            color = Color.Red,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
     )
 }
 
