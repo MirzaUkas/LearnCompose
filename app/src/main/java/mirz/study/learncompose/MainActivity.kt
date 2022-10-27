d002f7c961875b70f16900a71ded3e586d2c7419
Mirza Ukasyah <mirzauksyh@gmail.com>
2022-10-27 12:23:16 +0700

    Surface and Wrap Content Composables

--- app/src/main/java/mirz/study/learncompose/MainActivity.kt
+++ app/src/main/java/mirz/study/learncompose/MainActivity.kt
@@ -4,36 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import mirz.study.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearnComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingText("World")
-                }
-            }
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Surface(color = Color.DarkGray, modifier = Modifier.fillMaxSize()) {
+        Surface(color = Color.Magenta, modifier = Modifier.wrapContentSize()) {
+            Text(
+                text = "Wrapped Content",
+                style = MaterialTheme.typography.h3
+            )
         }
     }
 }
@@ -55,6 +54,6 @@ fun GreetingText(name: String) {
 @Composable
 fun DefaultPreview() {
     LearnComposeTheme {
-        GreetingText("World")
+        MainScreen()
     }
 }
